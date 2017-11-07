b1f31ecfcf103222d257f8b74a4431060e003dd4
Boris Grunwald <borisgrunwald@gmail.com>
2017-11-07 21:34:55 +0100

    Setup production build and server

--- src/app.js
+++ src/app.js
@@ -13,11 +13,6 @@ import 'react-dates/lib/css/_datepicker.css'
 
 const store = configureStore;
 
-
-
-const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-console.log(visibleExpenses);
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
