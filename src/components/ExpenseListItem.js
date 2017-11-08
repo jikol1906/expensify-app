d3dfefac30ac60561adc9be68f030d6787420218
Boris Grunwald <borisgrunwald@gmail.com>
2017-11-08 08:26:53 +0100

    setup formatting for amount and createdAt

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -2,13 +2,19 @@ import React from 'react';
 import { removeExpense } from "../actions/expenses";
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import moment from 'moment';
+import numeral from 'numeral';
 
 export default (props) => (
     <div>
         <NavLink to={"/edit/" + props.id}>
             <h3>{props.description}</h3>
         </NavLink>
-        <p>{props.amount} - {props.createdAt}</p>
+        <p>
+            {numeral(props.amount / 100).format('$0,0.00')}
+            -
+            {moment(props.createdAt).format('MMMM Do, YYYY')}
+        </p>
     </div>
 );
 
